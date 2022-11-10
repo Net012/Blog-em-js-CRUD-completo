3638f90c83cb3501c401fab69eae12e7c36882ce
José Prestes Neto <79334387+Net012@users.noreply.github.com>
2022-11-10 00:50:18 -0300

    Arquitetura e organização da aplicação
    Arquitetura e organização das pastas da aplicação utilizando o padrão MVC.

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express")
 
 const app = express();
 
-const port = 8080;
-
 const bodyParser = require("body-parser");
 
-const connection = require("./database/database")
+const connection = require("./database/database");
+
+const CategoriesController = require("./categories/CategoriesController");
+
+const ArticlesController = require("./articles/ArticlesController");
+
+const port = 8080;
+
 
 app.listen(port, () => {
     console.log("o servidor esta rodando na porta: " + port);
@@ -15,21 +20,29 @@ app.listen(port, () => {
 //Database 
 connection.authenticate()
     .then(() => {
-        console.log("Sucesso na conexão com o banco de dados")
+        console.log("Sucesso na conexão com o banco de dados");
     })
     .catch((error) => {
         console.log(error);
-    })
+    });
+
+
+//Rotas
+app.use("/", CategoriesController);
+
+app.use("/", ArticlesController);
+
+
 
 //view engine: ejs
 app.set("view engine", "ejs");
 
 //Static
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //body-parser
 app.use(bodyParser.urlencoded({ extend: false }));
 
 app.get("/", (req, res) => {
-    req.send("index")
+    req.send("index");
 });
