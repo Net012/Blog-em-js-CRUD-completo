02e348d419e22d25f40da0980d215171cdc04031
José Prestes Neto <79334387+Net012@users.noreply.github.com>
2022-11-10 04:53:55 -0300

    Criação do formulário de artigos com as operações CRUD

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,70 @@
 const express = require("express");
+
 const router = express.Router();
 
-router.get("/articles", (req,res)=>{
-    res.send("rota de artigos.");
+const Category = require("../categories/Category");
+
+const Article = require("./Article");
+
+const slugify = require("slugify");
+
+//rotas GET
+router.get("/admin/articles", (req,res)=>{
+
+    Article.findAll({
+        include: [
+            {model: Category}
+        ]
+    })
+    .then(articles =>{
+        res.render("admin/articles/index",{articles:articles});
+    });
 });
 
 router.get("/admin/articles/new", (req,res)=>{
-    res.send("rota admin para criar artigos.");
+
+    Category.findAll().then(categories=>{
+        res.render("admin/articles/new",{categories: categories}); 
+    });
+});
+
+//rotas POST
+router.post("/articles/save", (req,res)=>{
+    const article = {
+        title: req.body.title,
+        body: req.body.body,
+        category: req.body.category
+    };
+
+    Article.create({
+        title: article.title,
+        body: article.body,
+        slug: slugify(article.title),
+        categoryId: article.category 
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/delete",(req,res)=>{
+
+    const id = req.body.id;
+
+    if (id == undefined) {
+    
+        res.redirect("/admin/articles");
+        
+    } else {
+
+            Article.destroy({
+                where: {
+                    id:id
+                }
+            })
+            .then(()=>{
+                res.redirect("/admin/articles");
+            });
+    }
 });
 
 module.exports = router;
--- categories/Category.js
+++ categories/Category.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = require("../database/database");
 
 const Category = connection.define("categories",{
+    
     title: {
         type: Sequelize.STRING,
         allowNull: false
